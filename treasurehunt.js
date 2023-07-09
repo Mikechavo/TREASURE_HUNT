3c0bd7859c65b2826c1f194419721b7cac5487cb
Mikechavo <139020008+Mikechavo@users.noreply.github.com>
2023-07-09 03:00:10 -0500

    Update treasurehunt.js

--- treasurehunt.js
+++ treasurehunt.js
@@ -26,23 +26,3 @@ const bomb = Math.floor(Math.random()*8) + 1
 
 var countClicked = 0
 var countRemain = 4
-//const treasure = (location) => {
- // var randomNum = Math.floor(Math.random()*8)
-  //if(location === "Clue1") {
-  //document.getElementById("Clue1").innerHTML = "&#x1f332"
-  //} else if(location === "Clue2") {
-  //document.getElementById("Clue2").innerHTML = "&#x1f308"
-  //} else if(location === "Clue3") {
-  //document.getElementById("Clue3").innerHTML = "&#x1f332" 
-  //} else if(location === "Clue4") {
- // document.getElementById("Clue4").innerHTML = "&#x2620" 
-  //} else if(location === "Clue5") {
-  //  document.getElementById("Clue5").innerHTML = "&#x1f332" 
-  //} else if(location === "Clue6") {
-  //  document.getElementById("Clue6").innerHTML = "&#x1f332" 
-  //} else if(location === "Clue7") {
- //   document.getElementById("Clue7").innerHTML = "&#x1f332" 
-  //} else if(location === "Clue8") {
-  //  document.getElementById("Clue8").innerHTML = "&#x2620" 
-  //}
-//}  
